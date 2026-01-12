9a44cc918da79d50ac9e60312b07b08bfb3b7c5e
Sahith Akula <akulasahith268@gmail.com>
2026-01-13 00:04:50 +0530

    Changes

--- socket.js
+++ socket.js
@@ -1,5 +1,11 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://rd-timer-backend.onrender.com");
+const socket = io("https://rd-timer-backend.onrender.com", {
+  transports: ["websocket"],   // force WebSocket
+  reconnection: true,
+  reconnectionAttempts: Infinity,
+  reconnectionDelay: 1000,
+  timeout: 20000
+});
 
 export default socket;
